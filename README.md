05735c11952ab07636fe8e7060583cd12533b35c
Sriram Sridharan <ponds.2002@gmail.com>
2018-05-21 14:49:28 -0400

    Initial commit - added license, code and readme

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# rtl_433-based-Temperature-Sensor
-This projects show you how to use a rtl_sdr dongle with rtl_433, some temperature sensors, a Raspberry Pi and PRTG
+# YATS - Yet another temperature sensor using rtl_433
+
+I got bored with creating wireless temperature sensors with ESP8266s, so I started looking for cooler projects on a similar vein. A few weeks ago, I came across a project called [rtl_433](https://github.com/merbanan/rtl_433) that uses [rtl_sdr](http://sdr.osmocom.org/trac/wiki/rtl-sdr) to decode certain wireless signals sent over the 433MHz ISM band.
+
+I had previously seen references to rtl_sdr but did not pay much attention to the project. With the help of [RTL-SDR.com](https://www.rtl-sdr.com) and the [RTLSDR subreddit](https://www.reddit.com/r/RTLSDR/ "RTLSDR subreddit") I started reading up on both these projects and began my journey.
+
+The goal of this project is simple - take temperature and humidity data from a commercially available sensor, decode it and send it to a PRTG installation for storage and visualization.
+
+## Getting Started
+
+- A Raspberry Pi - I used a Raspberry Pi 3 Model B that I bought from Microcenter to use with the [AIY Voice Kit](https://aiyprojects.withgoogle.com/voice/ "AIY Voice Kit").
+- A micro-USB cable and a USB power supply (to power the Raspberry Pi and SDR dongle) - I used a OEM HTC charger that can do 2.4A but something [like this](https://www.raspberrypi.org/products/raspberry-pi-universal-power-supply/ "like this") would be the best.
+- A microSD card (atleast 8GB) loaded with a \*nix based OS  - I used Rasbian
+- rtl_sdr compatible dongle with appropriate antenna- I bought the [NooElec NESDR SMArt](https://smile.amazon.com/NooElec-NESDR-SMArt-Bundle-R820T2-Based/dp/B01GDN1T4S/ "NooElec NESDR SMArt") because it came with a 433MHz antenna (although a collapsible one will work just fine as well). [Here](http://www.radioforeveryone.com/p/july-6-2016-chinese-versus-branded.html "Here") is an article that might help you decide on a dongle.
+- Temperature sensors - I bought [this one](https://smile.amazon.com/AcuRite-06002M-Wireless-Temperature-Humidity/dp/B00T0K8NXC/ "this one") but you can get any sensor that uses a protocol supported by [rtl_433](https://github.com/merbanan/rtl_433/blob/master/README.md "rtl_433").
+
+### Prerequisites
+
+- A [PRTG](https://www.paessler.com/prtg "PRTG") install - you can likely adapt this proccess to other similar monitoring systems
+- Python 2.x (comes installed on Rasbian) - the script is untested on Python 3 and I understand that there are some urllib differences between the two versions
+
+### Installing
+
+#### Prepwork
+First update and upgrade all installed packages
+```Shell
+sudo apt update
+sudo apt upgrade
+```
+Then plug in your rtl_sdr dongle and check to see if it is detected
+```Shell
+lsusb
+```
+You should see something like `Bus 001 Device 004: ID 0bda:2838 Realtek Semiconductor Corp. RTL2838 DVB-T`
+
+Now check to see if dvb drivers are automatically loaded
+```Shell
+lsmod | grep dvb
+```
+If you see something like `dvb_usb_rtl28xxu`, that means that the default dvb drivers were installed and loaded. If that is the case, blacklist it by editing `/etc/modprobe.d/blacklist-dvb.conf`
+```Shell
+sudo nano /etc/modprobe.d/blacklist-dvb.conf
+```
+and adding `blacklist dvb_usb_rtl28xxu` to the file. If you're using nano to edit the file, like in my example, `ctrl-x` to get the save dialog and then hit `Y`.
+
+#### Installing rtl_sdr
+rtl_sdr has a few dependencies which may or may not be installed on your OS. Here are the dependencies in case they are not: `git-core`, `git`, `cmake`, `build-essential`, `libusb-1.0.0-dev`
+
+Now download rtl_sdr via git (I am assuming you are doing this in your home directory or you will need to modify step 8) and compile it.
+```Shell
+git clone git://git.osmocom.org/rtl-sdr.git
+mkdir build
+cd build/
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo ldconfig
+cd ~
+sudo cp ./rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
+```
+Now, reboot using `sudo shutdown -r now`.
+
+After rebooting use `rtl_test` to see if your rtl_dongle is detected properly and functioning normally.
+
+#### Installing rtl_433
+
+Simply follow [this well writted readme](https://github.com/merbanan/rtl_433/blob/master/README.md "this well writted readme") to install rtl_433.
+
+Okay, now let us test the whole set up so far. Mount the 433MHz antenna or collapse your telescopic antenna to quarter wavelength (~17.2cm, there's no need to be super precise). Now, run `rtl_433 -G`. This command will basically run through all supported protocols and try decode the received transmissions.
+
+You'll likely see something like [this](https://gist.github.com/sriram-sridharan/4da51490ebdbf512c3a9b9c18fb8d5cd "this") if you have one or more sensors operational (see the Prerequisites section).
+
+Finally, download my repo `git clone git@github.com:sriram-sridharan/rtl_433-based-Temperature-Sensor.git` and edit the configuration section of `main.py` with your settings and tokens.
+
+You can use [this guide](https://www.raspberrypi.org/documentation/linux/usage/rc-local.md) to make the script auto-run at boot.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Thanks
+
+* [rtl_sdr project](https://osmocom.org/projects/sdr/wiki/rtl-sdr)
+* [rtl_433 project](https://github.com/merbanan/rtl_433/)
+* [r/RTLSDR](https://www.reddit.com/r/RTLSDR/)
+
---
+++ main.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+import sys
+import os
+import json
+import urllib
+import urllib2
+import xml.etree.cElementTree as ET
+import time
+
+# ***************************** ***************************** #
+# ******************* Configuration - edit this ******************* #
+# I'm hardcoding sensor IDs to prevent reading other sensors that might be in the area.
+sensor1 = 0000
+sensor2 = 1111
+
+# PRTG Domain, port and sensor guid/token
+# <http or https>://<domain>:<port>
+prtg_settings = 'http://myprtg.com:8080'
+# sensor guid in PRTG
+prtg_token_sensor1 = "0000Token"
+prtg_token_sensor2 = "1111Token"
+
+# IFTTT settings (optional)
+# IFTTT Webhook Event Name
+event_name = "IFTTT%20EVENT"
+
+# IFTTT Webhook token from https://ifttt.com/services/maker_webhooks/settings
+ifttt_token = "IFTTTTOKEN"
+
+# rtl_433 arguments See https://github.com/merbanan/rtl_433/blob/master/README.md for flags
+rtl_433_arg = ' -q -W -R 40 -F json -C customary -T 20'
+# ******************* End config ******************* #
+# ************************* ************************* #
+
+# Average Function
+def list_avg (passed_array):
+	return sum(passed_array)/len(passed_array)
+
+# Create XML payload for GET request
+def prtg_payload (temperature, humidity):
+	root = ET.Element("prtg")
+	result = ET.SubElement(root, "result")
+	temp_xml = ET.SubElement(result, "channel")
+	temp_xml.text = "Temperature"
+	tempval_xml = ET.SubElement(result, "value")
+	tempval_xml.text = str(temperature)
+	float_xml = ET.SubElement(result, "float")
+	float_xml.text = str(1)
+	result2 = ET.SubElement(root, "result")
+	hum_xml = ET.SubElement(result2, "channel")
+	hum_xml.text = "Humidity"
+	humval_xml = ET.SubElement(result2, "value")
+	humval_xml.text = str(humidity)
+	float_xml = ET.SubElement(result2, "float")
+	float_xml.text = str(1)
+	return ET.tostring(root)
+
+
+while True:
+
+	# Initialize array here so that error reporting not affected by stale/previously stored values
+	# Array for sensor 1
+	Array_sensor1 = []
+	# Initialize the first to elements of Array_sensor1 to be arrays themselves
+	# [0] is temperature, [1] is humidity
+	Array_sensor1.append([])
+	Array_sensor1.append([])
+
+	# Array for sensor 2
+	Array_sensor2 = []
+	# Initialize the first to elements of Array_sensor2 to be arrays themselves
+	# [0] is temperature, [1] is humidity
+	Array_sensor2.append([])
+	Array_sensor2.append([])
+
+	# Read the current sensor data and clean
+	process = os.popen('rtl_433'+rtl_433_arg)
+	rtl_read = process.read()
+	process.close()
+	rtl_read = rtl_read.strip()
+	# SDR tuner is likely unplugged if there's no read data
+	if len(rtl_read)==0:
+		print >> sys.stderr, "The rtl_sdr tuner does not seem to be operational. Please make sure it is plugged in completely."
+		tuner_error_url = "https://maker.ifttt.com/trigger/" + event_name + "/with/key/" + ifttt_token + "?value1=" + "rtl_sdr Tuner"
+		# IFTTT error handling start
+		try:
+			contents = urllib2.urlopen(tuner_error_url).read()
+		except urllib2.HTTPError:
+			print >> sys.stderr, "Error connecting to IFTTT"
+		except urllib2.URLError:
+			print >> sys.stderr, "Error connecting to IFTTT"
+		# Sleep for longer since this is a hardware issue and requires user intervention
+		time.sleep(3600)
+		continue
+		# IFTTT error handling complete
+	rtl_prep = rtl_read.splitlines()
+	# Extract data
+	for eachline in rtl_prep:
+		encoded = json.loads(eachline)
+		if encoded["sensor_id"]==sensor1:
+			Array_sensor1[0].append(encoded["temperature_F"])
+			Array_sensor1[1].append(encoded["humidity"])
+		elif encoded["sensor_id"]==sensor2:
+			Array_sensor2[0].append(encoded["temperature_F"])
+			Array_sensor2[1].append(encoded["humidity"])
+	
+	# Error handling (out of battery, out of range, etc.) start
+	try:
+		# Print to console for debugging
+		print "Average Sensor 1 Temp is " + str(list_avg(Array_sensor1[0])) + "F"
+		print "Average Sensor 1 Humidity is " + str(list_avg(Array_sensor1[1])) + "%"
+		print "Average Sensor 2 Temp is " + str(list_avg(Array_sensor2[0])) + "F"
+		print "Average Sensor 2 Humidity is " + str(list_avg(Array_sensor2[1])) + "%"
+		living_url = prtg_settings + "/" + prtg_token_sensor1 + "?content=" + prtg_payload(list_avg(Array_sensor1[0]), list_avg(Array_sensor1[1]))
+		print "Living: " + living_url
+		bed_url = prtg_settings + "/" + prtg_token_sensor2 + "?content=" + prtg_payload(list_avg(Array_sensor2[0]), list_avg(Array_sensor2[1]))
+		print "Bed: " + bed_url
+	except ZeroDivisionError:
+		if len(Array_sensor1[0])==0 or len(Array_sensor1[1])==0:
+			print >> sys.stderr, "Sensor "+ str(sensor1) + " does not seem to be operational."
+			sensor1_error_url = "https://maker.ifttt.com/trigger/" + event_name + "/with/key/" + ifttt_token + "?value1=" + str(sensor1)
+			# IFTTT error handling start
+			try:
+				contents = urllib2.urlopen(sensor1_error_url).read()
+			except urllib2.HTTPError:
+				print >> sys.stderr, "Error connecting to IFTTT"
+			except urllib2.URLError:
+				print >> sys.stderr, "Error connecting to IFTTT"
+			# IFTTT error handling complete
+		elif len(Array_sensor2[0])==0 or len(Array_sensor2[1])==0:
+			print >> sys.stderr, "Sensor "+ str(sensor2) + " does not seem to be operational."
+			sensor2_error_url = "https://maker.ifttt.com/trigger/" + event_name + "/with/key/" + ifttt_token + "?value1=" + str(sensor2)
+			# IFTTT error handling start
+			try:
+				contents = urllib2.urlopen(sensor2_error_url).read()
+			
+			except urllib2.HTTPError:
+				print >> sys.stderr, "Error connecting to IFTTT"
+			except urllib2.URLError:
+				print >> sys.stderr, "Error connecting to IFTTT"
+			# IFTTT error handling complete
+	# Error handling (out of battery, out of range, etc.) complete
+	# PRTG error handling start
+	try:
+		contents = urllib2.urlopen(living_url)
+		contents = urllib2.urlopen(bed_url)
+	except urllib2.HTTPError:
+		print >> sys.stderr, "Error connecting to PRTG"
+	except urllib2.URLError:
+		print >> sys.stderr, "Error connecting to PRTG"
+	# PRTG error handling complete
+	time.sleep(45)
